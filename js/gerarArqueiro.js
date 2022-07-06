9ad40301998912031ba27a3471c34033f1f2f3c1
Luana Mendes <92415603+luuanamendes@users.noreply.github.com>
2022-07-06 20:20:17 -0300

    Add files via upload

---
+++ js/gerarArqueiro.js
@@ -0,0 +1,17 @@
+/*function gerarArqueiro(){
+    for(var i = 400; i <= 1000; i = i + 200){
+        var arqueiro = createSprite(i, 500, 20, 20);
+        arqueiro.shapeColor = "red";
+
+        gArqueiro.add(arqueiro);
+    }
+}*/
+
+function gerarArqueiro() {
+
+        arqueiro1 = createSprite(400, 530, 20, 20);
+        arqueiro1.shapeColor = "red";
+        //arqueiro1.velocityX = -2;
+    
+
+}
